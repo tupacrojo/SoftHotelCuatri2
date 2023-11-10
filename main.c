aace0c9262bbcb4a5981265552d46397b1a57d1f
juanxmartel <108769622+juanxmartel@users.noreply.github.com>
2023-11-10 18:42:17 -0300

    hotel

--- main.c
+++ main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+int piso;
+int cantidadHabitaciones;
+int idHabitacion;
+int cantidadHuesped;
+int ocupado;
+int habilitada;
+}pisoRegistro;
+
+typedef struct {
+    int idHabitacion;
+    int cantidadHuesped;
+    int ocupado;
+    int habilitada;
+}stHabitacion;
+
+typedef struct nodoListaHabitacion {
+struct nodoListaHabitacion * sig;
+stHabitacion dato;
+}nodoListaHabitacion;
+
 int main()
 {
-    printf("Hello world!\n");
+    int arregloDePisos[15] =cargarArregloDePisos();
+
     return 0;
 }
+
+
+
