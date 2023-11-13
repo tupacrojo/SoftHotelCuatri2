59150dbdc92655c9977989b572b343cd093b6a61
Lopee12 <nlopezosornio@gmail.com>
2023-11-12 23:22:02 -0300

    Arreglo de Structs

--- main.c
+++ main.c
@@ -4,68 +4,102 @@
 #include <conio.h>
 #include <string.h>
 
-typedef struct ///  "stReserva" reserva
-{
+///ESTRUCTURA DE CARACTERISTICAS
+
+typedef struct{
+
+    int id;
+    int borrado;
+    char tipo[30];
+
+}stRegCaracteristicas;
+
+///ESTRUCTURA DE HABITACIONES
+
+typedef struct{
+
+    int id;
+    int borrado;
+    int estado;
+    int piso;
+    stRegCaracteristicas caracteristicas[10];
+
+}stRegHabitacion;
+
+///ESTRUCTURA DE LA RESERVA
+
+typedef struct{
+
     stRegHabitacion habitacion;
     int borrado;
-    // entrada
+    /// ENTRADA
     int diaEntrada;
     int mesEntrada;
     int anioEntrada;
-    // salida
+    /// SALIDA
     int diaSalida;
     int mesSalida;
     int anioSalida;
+
 }stReserva;
 
-typedef struct /// "stUsuario" usuario
-{
-    int id;   // auto incrementable
-    int borrado;
-    int tipo; // GodMan(), administrador(sueldos, dar de alta y baja empleado), recepcionista(Check In, Check Out), Huesped(consumos)
+///ESTRUCTURA DE USUARIO
+
+typedef struct{
+
+    int id;
+    int borrado; ///PREGUNTAR
+    int tipo;
     char contrasena[20];
-    char nombre[50]; // lo que va antes del apellido
+    char nombre[50];
     char apellido[50];
     char DNI[10];
     float totalconsumo;
     stReserva reserva;
-    int edad; // a os
+    int edad;
     char telefono[20];
-    char genero; // m = masculino , f = femenino, o = otros
+    char genero;
     int hijos;
-    char estadoCivil[20]; // 0 soltero, 1 divorciado, 2 viudo, 3 casado
+    char estadoCivil[20];
+
 }stUsuario;
 
-typedef struct //este es la estructura que alamcena las caracteristicas de la habitacion
-{
-    int id;
-    int borrado;
-    char tipo[30]; // vista al mar, cama matrinomial, perroincluido
-}stRegCaracteristicas;
+///ESTRUCTURA DE REGISTRO DE SERVICIOS
 
-typedef struct ///  "stRegHabitacion" habitacion
-{
-    int id;
-    int borrado;
-    int estado;  // 0 libre; 1 Ocupado;
-    int piso;   // pisos
-    stRegCaracteristicas caracteristicas[10];// este arreglo puede almacenar
-}stRegHabitacion;
+typedef struct{
 
-typedef struct ///  "stConsumibles" elementos disponibles para consumo
-{
-    int id;
-    int borrado;
-    char nombre[50];
-    char categoria[50]; // Bebible, Comestible, Servicios
-    float precio;
-}stConsumibles;
+    int IdServicio;
+    char Servicio[25];
+    char SubServicio[25];
+    float Precio;
+
+}StRegistroServicio;
+
+///ESTRUCTURA SUB SERVICIO
+
+typedef struct{
+
+    int Borrado;
+    char SubServicio[25];
+    float Precio;
+
+}StSubServicio;
+
+///ESTRUCTURAS DE SERVICIOS
+
+typedef struct{
+
+    int IdServicio;
+    int Borrado;
+    char Servicio[25];
+
+}StServicio;
 
 ///ESTRUCTURA DE LA LISTA SECUNDARIA DE SERVICIO
 
 typedef struct NodoS{
 
-    char Subservicio[25];
+    StSubServicio Dato;
     struct NodoS*siguiente;
 
 }NodoS;
@@ -74,7 +108,7 @@ typedef struct NodoS{
 
 typedef struct NodoP{
 
-    char Servicio[25];
+    StServicio Dato;
     NodoS * lista;
     struct NodoP*siguiente;
 
@@ -85,12 +119,19 @@ typedef struct NodoP{
 ///LISTA PRINCIPAL
 
 NodoP * InicListaPrincipal();
-NodoP * CrearNodoServicio(char Servicio[]);
+NodoP * CrearNodoServicio(StServicio Dato);
+NodoP * AgregarAlPcpioListaServicio (NodoP* lista, NodoP* nuevo);
 
 ///LISTA SECUNDARIA
 
 NodoS * InicListaSecundaria();
-NodoS * CrearNodoSubservicio(char Subservicio[]);
+NodoS * CrearNodoSubservicio(StSubServicio Dato);
+NodoS * AgregarAlPcpioListaSubservicio (NodoS* lista, NodoS* nuevo);
+
+///FUNCIONES DE ESTRUCTURA COMPUESTA
+
+NodoP* CargarListaServicioConArchivo(NodoP * lista);
+NodoP* BuscarServicio(NodoP * lista, char Servicio[]);
 
 
 int main()
@@ -116,12 +157,12 @@ return NULL;
 
 ///FUNCION PARA CREAR UN NODO DE LA LISTA PRINCIPAL
 
-NodoP * CrearNodoServicio(char Servicio[]){
+NodoP * CrearNodoServicio(StServicio Dato){
 
     NodoP*nuevo = (NodoP*)malloc(sizeof(NodoP));
 
     nuevo->lista = InicListaSecundaria();
-    strcpy(nuevo->Servicio,Servicio);
+    nuevo->Dato =  Dato;
     nuevo->siguiente = NULL;
 
 return nuevo;
@@ -129,16 +170,83 @@ return nuevo;
 
 ///FUNCION PARA CREAR UN NODO DE LA LISTA SECUNDARIA
 
-NodoS * CrearNodoSubservicio(char Subservicio[]){
+NodoS * CrearNodoSubservicio(StSubServicio Dato){
 
     NodoS*nuevo = (NodoS*)malloc(sizeof(NodoS));
 
     nuevo->siguiente = NULL;
-    strcpy(nuevo->Subservicio,Subservicio);
+    nuevo->Dato = Dato;
 
 return nuevo;
 }
 
 ///FUNCION PARA AGREGAR UN SERVICIO AL INICIO DE LA LISTA
+
+NodoP * AgregarAlPcpioListaServicio (NodoP* lista, NodoP* nuevo){
+
+    if(lista == NULL){
+        lista = nuevo;
+    }else{
+        nuevo->siguiente = lista;
+        lista = nuevo;
+    }
+return lista;
+}
+
+///FUNCION PARA AGREGAR UN SUB SERVICIO AL INICIO DE LA LISTA
+
+NodoS * AgregarAlPcpioListaSubservicio (NodoS* lista, NodoS* nuevo){
+
+    if(lista == NULL){
+        lista = nuevo;
+    }else{
+        nuevo->siguiente = lista;
+        lista = nuevo;
+    }
+return lista;
+}
+
+///FUNCIONES PARA CARGAR DATOS DENTRO DE LA ESTRUCTURA COMPUESTA
+
+///FUNCION PARA CARGAR LA LISTA CON LOS DATOS DEL ARCHIVO
+
+NodoP* CargarListaServicioConArchivo(NodoP * lista){
+
+    StRegistroServicio Aux;
+
+    FILE * archi = fopen(Arreglo,"rb");
+
+    if(archi!=NULL){
+        while(fread(&Aux,sizeof(StRegistroServicio),1,archi)>0){
+            lista = AltaDeRegistroServicio(lista,Aux);
+        }
+
+        fclose(archi);
+    }else{
+        printf("El archivo se abrio incorrectamente\n");
+    }
+
+return lista;
+}
+
+
+///FUNCION PARA DAR EL ALTA DE UN REGISTRO DE SERVICIOS
+
+///FUNCION PARA BUSCAR UN SERVICIO
+
+NodoP* BuscarServicio(NodoP * lista, char Servicio[]){
+
+    NodoP*Aux = NULL;
+    NodoP*seg = lista;
+
+    while(seg != NULL && Aux == NULL){
+        if(strcmpi(seg->Servicio,Servicio) == 0){
+            Aux = seg;
+        }
+        seg = seg->siguiente;
+    }
+
+return Aux;
+}
 
 
