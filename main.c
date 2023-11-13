661634d4e8b88ce7fb9e778478b7452189258e15
mariano <mariano.maffioni10@gmail.com>
2023-11-13 19:38:08 -0300

    primer funcion
    hice la funcion de cargar el arreglo de listas.

--- main.c
+++ main.c
@@ -1,17 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
 
+char nombre[] = {"habitaciones.bin"}
+
+
+typedef struct //este es la estructura que alamcena las caracteristicas de la habitacion
+{
+    char tipo[30]; // vista al mar, cama matrinomial, perroincluido
+}stRegCaracteristicas;
+
+
+typedef struct ///  "stRegHabitacion" habitacion
+{
+    int estado;  // 0 libre; 1 Ocupado;
+    int piso;   // pisos
+    char tipoDeHabitacion [8];
+    struct NodoPiso * lista;
+    stRegCaracteristicas caracteristicas[10];// este arreglo puede almacenar
+
+
+}stRegHabitacion;
+
+
+
+
+
+typedef struct {
+    stRegHabitacion DATOS;
+
+    struct NodoPiso * siguiente;
+}NodoPiso;
 
 
 
 int main()
 {
-    printf("TODAS PUTAS\n");
+    stRegHabitacion ARREGLO[12];
+    int x = 0;
+    FILE * archi = fopen(nombre,"rb");
+    while (x < 12){
+        ARREGLO [x] = CargarElArreglo(ARREGLO[],x,archi);
+        x++;
+    }
+
+    fclose(archi);
+
+
+
     return 0;
 }
 
-void mostrar (){
 
-int x;
+NodoPiso * iniclista (){
+    return NULL;
 }
 
+NodoPiso * crearNodo (stRegHabitacion DATO){
+    NodoPiso * aux = (NodoPiso *) malloc(sizeof (NodoPiso));
+
+    aux->DATOS = DATO;
+    aux->siguiente = NULL;
+return aux;
+}
+
+NodoPiso * agregarALista(NodoPiso * lista, NodoPiso * DATO){
+    if (lista == NULL){
+        lista = DATO;
+    }else{
+        DATO->siguiente = lista;
+        lista = DATO;
+    }
+return lista;
+}
+
+
+stRegHabitacion CargarElArreglo(stRegHabitacion array[], int posArray, FILE * archi) { /// el array de los pisos y le paso el dato del archivo
+    int i = 0;
+    stRegHabitacion  ayudin;
+    array[posArray].lista = iniclista();
+    while (i < 5){ /// insertamos 5 habitaciones en la posision del arreglo determinada
+        fread(&ayudin,sizeof(stRegHabitacion),1,archi); /// leo la habitacion
+
+        NodoPiso * aux = crearNodo (ayudin);
+        array[posArray].lista = agregarALista (array[posArray].lista,aux); /// inserto en la lista del array [posArray]
+
+        i++;
+    }
+
+return array[posArray]; /// aca quiero retornar la el arreglo en la posArray
+}
