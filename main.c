9f3c5aec6b8a7e7f4199072418c6550f3fbe85e5
mariano <mariano.maffioni10@gmail.com>
2023-11-13 22:50:02 -0300

    cambios de estructuras
    cambie las estructuras, y es probable que tengamos q crear un nuevo archivo con las características de la habitaciones

--- main.c
+++ main.c
@@ -3,49 +3,41 @@
 #include <malloc.h>
 #include <string.h>
 
-char nombre[] = {"habitaciones.bin"}
-
-
-typedef struct //este es la estructura que alamcena las caracteristicas de la habitacion
-{
-    char tipo[30]; // vista al mar, cama matrinomial, perroincluido
-}stRegCaracteristicas;
-
-
-typedef struct ///  "stRegHabitacion" habitacion
-{
-    int estado;  // 0 libre; 1 Ocupado;
-    int piso;   // pisos
-    char tipoDeHabitacion [8];
-    struct NodoPiso * lista;
-    stRegCaracteristicas caracteristicas[10];// este arreglo puede almacenar
-
-
-}stRegHabitacion;
-
-
-
 
 
 typedef struct {
-    stRegHabitacion DATOS;
+    int id;
+    char caracteristicas[25];
+    int cantCamas;
+    char tipoDeCama[13]; // simple, matrimonial o combinada
+}habitacion;
+
+typedef struct {
+    habitacion DATOS;
 
     struct NodoPiso * siguiente;
 }NodoPiso;
+
+typedef struct {
+    int idPiso;
+    NodoPiso * lista;
+}stDeCelda;
+
+typedef struct {
+    int id;
+    char caracteristicas[25];
+    int cantCamas;
+    char tipoDeCama[13]; // simple, matrimonial o combinada
+    int idPiso;
+}archivo;
+
+
 
 
 
 int main()
 {
-    stRegHabitacion ARREGLO[12];
-    int x = 0;
-    FILE * archi = fopen(nombre,"rb");
-    while (x < 12){
-        ARREGLO [x] = CargarElArreglo(ARREGLO[],x,archi);
-        x++;
-    }
 
-    fclose(archi);
 
 
 
@@ -57,7 +49,7 @@ NodoPiso * iniclista (){
     return NULL;
 }
 
-NodoPiso * crearNodo (stRegHabitacion DATO){
+NodoPiso * crearNodo (habitacion DATO){
     NodoPiso * aux = (NodoPiso *) malloc(sizeof (NodoPiso));
 
     aux->DATOS = DATO;
@@ -76,18 +68,3 @@ return lista;
 }
 
 
-stRegHabitacion CargarElArreglo(stRegHabitacion array[], int posArray, FILE * archi) { /// el array de los pisos y le paso el dato del archivo
-    int i = 0;
-    stRegHabitacion  ayudin;
-    array[posArray].lista = iniclista();
-    while (i < 5){ /// insertamos 5 habitaciones en la posision del arreglo determinada
-        fread(&ayudin,sizeof(stRegHabitacion),1,archi); /// leo la habitacion
-
-        NodoPiso * aux = crearNodo (ayudin);
-        array[posArray].lista = agregarALista (array[posArray].lista,aux); /// inserto en la lista del array [posArray]
-
-        i++;
-    }
-
-return array[posArray]; /// aca quiero retornar la el arreglo en la posArray
-}
