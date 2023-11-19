9a4de87f50b3a39b655265bfa0e222b99b4c3f13
mariano <mariano.maffioni10@gmail.com>
2023-11-19 11:42:59 -0300

    cambios finales

--- src/lib/caracteristicas.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-typedef struct //este es la estructura que alamcena las caracteristicas de la habitacion 
-{
-    int id;
-    int borrado;
-    char tipo[30]; // vista al mar, cama matrinomial, perroincluido
-}stRegCaracteristicas;
