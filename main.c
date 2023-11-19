293f3bf1fd29cc6f3ecea1db67a3899d3a5883ca
tupacrojo <tupacrojo@gmail.com>
2023-11-19 11:36:08 -0300

    dfsa

--- main.c
+++ main.c
@@ -142,45 +142,7 @@ typedef struct nodoListaHabitacion
 
 int main()
 {
-    int arregloDePisos[15] = cargarArregloDePisos();
-    int validos = 0;
-    celdaEvento eventos[11];
-    //cargarArchivo();
-
-    /// FUNCION PARA QUE EL USUARIO CARGUE UN SOLO EVENTO (Aca lo cargo en el archivo)
-   // funcionCargarEvento();
-    /// FUNCION PARA CARGAR EL ARREGLO:
-    validos = pasarDatosDeArchivoAarregloEventos(eventos, 11, validos);
-    /// FUNCION PARA MOSTRAR EL ARREGLO COMPLETO:
-    mostrarMesyDias(eventos, validos);
-    /// FUNCION PARA VER LOS EVENTOS DE UN MES:
-    funcionVerEventosMes(eventos, validos);
-    /// FUNCION PARA BUSCAR SI UNA FECHA ESTA DISPONIBLE PARA UN EVENTO:
-    funcionBuscarFechaDispo(eventos, validos);
-    /// FUNCION PARA SUMAR LA CANTIDAD DE EVENTOS DE UN MES:
-    funcionSumarCantEventosMes(eventos, validos);
-    /// FUNCION PARA VER LAS GANANCIAS DE UN MES:
-    funcionSumarGananciasMes(eventos, validos);
-    /* /// FUNCION PARA BORRAR UN EVENTO DE UN MES:
-      funcionBorrarEvento(eventos,validos);
-    */
-    /*
-    //existeArchivo(aUsuarios);
-    nodoLD *lista = inicListaDoble();
-    //agregarUsuarioArchivo();
-    //crearReserva(buscarUsuarioDocumento("44860125"));
-    lista = cargarListaDobleArchivo(lista,aUsuarios);
-    imprimirDesdePrincipio(lista);
-    ///CargarUnArchivo();
-<<<<<<< Updated upstream
-    ///MostrarUnArchivo();
-=======
-    ///MostrarUnArchivoServicios();
->>>>>>> Stashed changes
-    MenuServicios();
-    */
-    crearBackupEscritorio();
-
+ printf("Hello World");
     return 0;
 }
 
